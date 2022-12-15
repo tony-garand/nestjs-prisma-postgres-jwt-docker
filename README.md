e4711d64e9c30e9e61c2df23c62cd2200a272756
Anthony Garand <anesthetics1@gmail.com>
2022-12-15 06:07:33 -0800

    init

---
+++ README.md
@@ -0,0 +1,68 @@
+<p align="center">
+  <a href="https://docs.nestjs.com/" target="_blank"> <img src="https://repository-images.githubusercontent.com/180571949/1816f680-a221-11ea-8ae0-8d90df8436bb" alt="Nest Logo" /></a>
+</p>
+
+## Description
+
+- NestJS
+- Prisma
+- PostgreSQL
+- Swagger
+- JWT
+- Docker
+
+# Running the app on docker
+
+## Docker build & start
+
+```bash
+# docker env build
+$ docker-compose build
+
+# docker env start
+$ docker-compose up
+
+# remove docker container (services & networks)
+$ docker-compose down
+```
+
+## Migration
+
+```bash
+# generate migration
+$ docker-compose run nestjs npm run typeorm:generate AnyNameYouLike
+
+# run migration
+$ docker-compose run nestjs npm run typeorm:run
+```
+
+# Running the app without docker
+
+## Installation
+
+```bash
+$ npm install
+```
+
+## Migration
+
+```bash
+# generate migration
+$ npm run typeorm:generate AnyNameYouLike
+
+# run migration
+$ npm run typeorm:run
+```
+
+## Running the app
+
+```bash
+# development
+$ npm run start
+
+# watch mode
+$ npm run start:dev
+
+# production mode
+$ npm run start:prod
+```
---
+++ src/main.ts
@@ -0,0 +1,42 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Request } from 'express';
+import * as cookieParser from 'cookie-parser';
+import * as csurf from 'csurf';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors({
+    credentials: true,
+    origin: ['http://localhost:3000'],
+  });
+
+  const options = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Datawell')
+    .setVersion('1.0')
+    .addTag('DatawellAPI')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  app.use(cookieParser());
+  app.use(
+    csurf({
+      cookie: {
+        httpOnly: true,
+        sameSite: 'none',
+        secure: true,
+      },
+      value: (req: Request) => {
+        return req.header('csrf-token');
+      },
+    }),
+  );
+  await app.listen(process.env.PORT || 3005);
+}
+bootstrap();
